29cd4fa45ba91313b906cd1fd5ee69f3a8a2b320
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2021-03-21 20:00:16 -0300

    Criação e implementação da camada de serviços e da modelo

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TarefaService} from './shared';
+import { ListarTarefasComponent } from './listar'; 
+
+@NgModule({
+  declarations: [ListarTarefasComponent],
+  imports: [
+    CommonModule
+  ],
+  providers:[TarefaService]
+})
+export class TarefasModule { }
