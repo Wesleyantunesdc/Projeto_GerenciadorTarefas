f661f0850458224c03c3f4155505af454a4b4be8
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2021-03-22 21:32:16 -0300

    Finalização do projeto.

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TarefaService} from './shared';
+import { TarefaService, TarefaConcluidaDirective} from './shared';
 import { ListarTarefasComponent } from './listar'; 
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CadastrarTarefasComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
  
 @NgModule({
-  declarations: [ListarTarefasComponent, CadastrarTarefasComponent],
+  declarations: [ListarTarefasComponent, CadastrarTarefasComponent, EditarTarefaComponent, TarefaConcluidaDirective],
   imports: [
     CommonModule,
     RouterModule,
